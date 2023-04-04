96c1295cd10efd7eeabc308f3fdc459025bff1b8
penach0 <josemanuel.penacho@gmail.com>
2023-04-04 10:48:05 +0100

    Set root to login page
    - This was a bit more complicated than expected, I needed to set the
    root route inside a "devise_scope :user" block

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    root "devise/sessions#new"
+  end
+
   devise_for :users
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end

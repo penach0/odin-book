8eb90dbad6b141e35c6f4994b90c23500c884299
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 18:23:27 +0100

    Create Profile and User at the same time
    - Moved the profile creation to the devise registration controller so
    it happens at the same time as the user creation, this cleans up the
    profile controller a bit
    - Create #set_variables (not a great name) in the profile controller.
    With a before_action hook this method avoids a lot of duplication

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,18 +1,11 @@
 class ProfilesController < ApplicationController
-  def show
-    @user = User.find(params[:user_id])
-    @profile = @user.profile
-  end
+  before_action :set_variables
 
-  def edit
-    @user = User.find(params[:user_id])
-    @profile = @user.profile || @user.build_profile
-  end
+  def show; end
+
+  def edit; end
 
   def update
-    @user = User.find(params[:user_id])
-    @profile = @user.profile || @user.create_profile
-
     if @profile.update(profile_params)
       redirect_to [@user, @profile]
     else
@@ -23,5 +16,10 @@ class ProfilesController < ApplicationController
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :date_of_birth, :bio)
+    end
+
+    def set_variables
+      @user = User.find(params[:user_id])
+      @profile = @user.profile
     end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -10,9 +10,10 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+    resource.create_profile
+  end
 
   # GET /resource/edit
   # def edit
@@ -51,9 +52,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    edit_user_profile_path(resource)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)

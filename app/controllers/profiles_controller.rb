781074b934b9539bfd2da156da0ded45608cb188
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 17:50:44 +0100

    Create strong params for Profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,6 +6,11 @@ class ProfilesController < ApplicationController
 
   def edit
     @user = User.find(params[:user_id])
-    @profile = @user.profile || @user.create_profile
+    @profile = @user.profile || @user.build_profile
   end
+
+  private
+    def profile_params
+      params.require(:profile).permit(:first_name, :last_name, :date_of_birth, :bio)
+    end
 end

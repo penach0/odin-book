38baf65df0d6f32a5500bc3e653a3f305e394288
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 19:44:37 +0100

    Update users index view and nav bar
    - Users are now listed by name and not by email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all_except(current_user)
+    @users = User.all_except(current_user).includes(:profile)
   end
 
   def show
+    @user = User.find(params[:id])
+    @profile = @user.profile
   end
 end

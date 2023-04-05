ffc2aee9c37c433405ce92bcdb8b9b9c1414360a
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 08:33:21 +0100

    Create User#active_friends
    - This method solves the previously mentioned issue by concatenating
    a users friends with his inverse friends

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,8 @@ class User < ApplicationRecord
   has_many :friends, -> { Friendship.accepted }, through: :friendships
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
   has_many :inverse_friends, -> { Friendship.accepted }, through: :inverse_friendships, source: :user
+
+  def active_friends
+    friends + inverse_friends
+  end
 end

2347e9b82dc1953672a15a5fce9a69ae9b10c5f8
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 10:38:17 +0100

    Build Friendships controller create action
    - This action correctly creates new friendship between the current user
    and the user picked
    - The link to send requests is now linked to this action and sends the
    picked user id as a parameter
    - Some current problems:
      - The current user shouldn't be displayed in the list of all users
      - Send request link needs to disappear after a sent request (since
      I have harsh constraints in the db duplicating a request breaks
      everything)

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,4 +1,11 @@
 class FriendshipsController < ApplicationController
   def create
+    friendship = current_user.friendships.build(friend_id: params[:friend_id])
+
+    if friendship.save
+      redirect_back(fallback_location: root_path)
+    else
+      render '/', status: :unprocessable_entity
+    end
   end
 end

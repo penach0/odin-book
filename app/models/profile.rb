b6b57b91c106c35bbf7926b02519cd8cd4974285
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 18:35:08 +0100

    Add validations of presence to Profile
    - These validations are only enforced in updates since the profile is
    created empty

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,13 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  validates :first_name, :last_name, :date_of_birth, :bio, presence: true, on: :update
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+
+  def age
+    ((Time.zone.now - date_of_birth.to_time) / 1.year.seconds).floor
+  end
 end

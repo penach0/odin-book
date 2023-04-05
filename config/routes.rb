bbe9edc412b8c4fb300366b2a8df5df47a1c974a
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 10:25:43 +0100

    Generate Friendships controller
    - Only has a create action
    - Route is currently nested in the users routes, not sure if it is a
    good idea if anything seems wrong I will pull them out

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'friendships/create'
   devise_for :users
 
   devise_scope :user do
     root "users#index"
   end
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    resources :friendships, only: :create
+  end
 end

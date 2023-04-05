d4ed4750d767fdc467d0fc841cdd979d3b059153
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 08:41:51 +0100

    Generate Users controller
    - I want to start having views and actions to build sending and
    accepting friend requests functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
   devise_scope :user do
-    root "devise/sessions#new"
+    root "users#index"
   end
 
   devise_for :users
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-
+  resources :users, only: [:index, :show]
 end

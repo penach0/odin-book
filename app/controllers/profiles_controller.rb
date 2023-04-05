34a775860f32c08a755e0c544d7654e7e21548e2
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 17:43:45 +0100

    Create Profiles #show action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,9 @@
 class ProfilesController < ApplicationController
+  def show
+    @user = User.find(params[:user_id])
+    @profile = @user.profile
+  end
+
   def edit
     @user = User.find(params[:user_id])
     @profile = @user.profile || @user.create_profile

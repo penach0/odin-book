4b42947ea27a1509d2be23b6c9163c927209f5a0
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 19:15:19 +0100

    Make first and last name required
    - Decided I want to force users to give me first and last name at least
    so I can display them in lists of users and such, so the "Edit later"
    link only appears for users who have already given first and last name
    (this was updated in the view with the Profile#required_info? method)
    - Date of birth and bio are now optional but the user will be prompted
    to update them every time they log in (still to be implemented).
    - Profile#age runs a check for the existence of a date of birth before
    doing anything else

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,17 +1,23 @@
 class Profile < ApplicationRecord
   belongs_to :user
 
-  validates :first_name, :last_name, :date_of_birth, :bio, presence: true, on: :update
+  validates :first_name, :last_name, presence: true, on: :update
 
   def full_name
     "#{first_name} #{last_name}"
   end
 
   def age
+    return unless date_of_birth
+
     ((Time.zone.now - date_of_birth.to_time) / 1.year.seconds).floor
   end
 
   def incomplete?
     attributes.values.any?(&:nil?)
+  end
+
+  def required_info?
+    [first_name, last_name].none?(&:blank?)
   end
 end

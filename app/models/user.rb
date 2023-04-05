1e9f813bfd3a54ce4d76ee6e1b745e4053d98a5c
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 11:03:32 +0100

    Remove "Send Request" link from create friendships
    - Create two helper methods on the User model to check if another user
    is either already a friend or has already received a request. This
    methods are used in the Users#index view to set appropriate messages or
    links depending on the relationship status

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,21 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :friendships
-  has_many :friends, -> { Friendship.accepted }, through: :friendships
   has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
+  has_many :friends, -> { Friendship.accepted }, through: :friendships
   has_many :inverse_friends, -> { Friendship.accepted }, through: :inverse_friendships, source: :user
+  has_many :sent_requests, -> { Friendship.pending }, through: :friendships, source: :friend
+  has_many :received_requests, -> { Friendship.pending }, through: :inverse_friendships, source: :user
 
   def active_friends
     friends + inverse_friends
+  end
+
+  def request_sent?(other)
+    sent_requests.include?(other)
+  end
+
+  def friend?(other)
+    active_friends.include?(other)
   end
 end

1ad04975d18267dda7912c0e2f703aa9a61b19f0
penach0 <josemanuel.penacho@gmail.com>
2023-04-05 18:07:46 +0100

    Create #update action for Profiles

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,6 +9,17 @@ class ProfilesController < ApplicationController
     @profile = @user.profile || @user.build_profile
   end
 
+  def update
+    @user = User.find(params[:user_id])
+    @profile = @user.profile || @user.create_profile
+
+    if @profile.update(profile_params)
+      redirect_to [@user, @profile]
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :date_of_birth, :bio)

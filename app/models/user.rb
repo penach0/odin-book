15548f8b6b97567f02ff13f977f93b83a53cf3ec
penach0 <josemanuel.penacho@gmail.com>
2023-04-06 08:47:05 +0100

    Set up association between User and Post

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,14 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :friendships
-  has_many :inverse_friendships, class_name: "Friendship", foreign_key: :friend_id
+  has_many :inverse_friendships, foreign_key: :friend_id, class_name: "Friendship"
   has_many :friends, -> { Friendship.accepted }, through: :friendships
   has_many :inverse_friends, -> { Friendship.accepted }, through: :inverse_friendships, source: :user
   has_many :sent_requests, -> { Friendship.pending }, through: :friendships, source: :friend
   has_many :received_requests, -> { Friendship.pending }, through: :inverse_friendships, source: :user
 
   has_one :profile, dependent: :destroy
+  has_many :created_posts, foreign_key: :creator_id, class_name: "Post"
 
   scope :all_except, ->(user) { where.not(id: user.id) }
 

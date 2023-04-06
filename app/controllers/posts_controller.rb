8e22275a0f79eba73139cdf9d6236bd35bcee5a2
penach0 <josemanuel.penacho@gmail.com>
2023-04-06 20:31:36 +0100

    Create Post model scope :ordered
    - This scope orders the Posts in descending creation time order. This
    is useful for displaying posts in the correct timeline on the user feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @posts = Post.ordered
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :creator, class_name: "User"
 
   validates :body, presence: true
+
+  scope :ordered, -> { order(created_at: :desc) }
 end

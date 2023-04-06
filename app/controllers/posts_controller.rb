7ef09207a0b5a85bcb9e459c711667fda06cbe0b
penach0 <josemanuel.penacho@gmail.com>
2023-04-06 09:10:22 +0100

    Create Posts #show and #create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,26 @@
 class PostsController < ApplicationController
+  def show
+    @post = Post.find[:post_id]
+  end
+
   def new
     @user = current_user
     @post = Post.new
   end
+
+  def create
+    @user = current_user
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:body)
+    end
 end

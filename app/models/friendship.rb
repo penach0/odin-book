0f37ba6d0489b5509fdeb3abe18b5289b2f9f5b7
penach0 <josemanuel.penacho@gmail.com>
2023-04-08 20:34:35 +0100

    Create Friendship.find_interchangeable
    - This method is useful to find a friendship without worrying about
    who is the requester and who is the requestee

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -6,4 +6,9 @@ class Friendship < ApplicationRecord
 
   scope :accepted, -> { where(status: :accepted) }
   scope :pending, -> { where(status: :pending) }
+
+  def self.find_interchangeable(first_user, second_user)
+    where(user_id: first_user, friend_id: second_user)
+      .or(where(user_id: second_user, friend_id: first_user))
+  end
 end

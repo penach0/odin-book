584d2b246532ac2393926e0df9c5379e38d6351d
penach0 <josemanuel.penacho@gmail.com>
2023-04-08 18:35:30 +0100

    Remove "rejected" status from Friendship enum
    - Rejected requests will be destroyed from the database. This change
    happens because I was considering the implications of having a
    friendship with a "rejected" status on the database. One of the
    difficulties would be if the request was then to be sent from the other
    direction (from the rejecter). In this situation I would have a
    "requestee" sending a request and even though it could be made to work
    there is no need to go this route. If I later want a rejected status
    (useful maybe to keep track of rejection count) I might separate
    friendships from requests

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,7 +2,7 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: "User"
 
-  enum :status, [:pending, :accepted, :rejected]
+  enum :status, [:pending, :accepted]
 
   scope :accepted, -> { where(status: :accepted) }
   scope :pending, -> { where(status: :pending) }

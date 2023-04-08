a04bf0cca765d793710511b8603677a7c16486ad
penach0 <josemanuel.penacho@gmail.com>
2023-04-08 14:43:34 +0100

    Include :creator in the @posts var query
    - Suggested by the bullet gem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
   before_action :set_user, except: [:index, :show]
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  before_action -> { authorize_user(Post.find(params[:id])) }, only: [:edit, :update, :destroy]
+  # before_action -> { authorize_user(Post.find(params[:id])) }, only: [:edit, :update, :destroy]
 
   def index
-    @posts = Post.ordered
+    @posts = Post.includes(:creator).ordered
   end
 
   def show; end
@@ -29,6 +29,7 @@ class PostsController < ApplicationController
   def edit; end
 
   def update
+    authorize @post
     if @post.update(post_params)
       redirect_to @post
     else

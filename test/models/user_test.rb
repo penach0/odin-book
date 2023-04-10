8aaa90ed1800ecd37d25546bb200f9bfc0056f3e
penach0 <josemanuel.penacho@gmail.com>
2023-04-10 20:25:54 +0100

    Start testing
    - Created fixtures
    - Tested validations for user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'valid user' do
+    user = users(:one)
+    assert user.valid?
+  end
+
+  test 'invadid without email' do
+    user = users(:one)
+    user.email = ""
+
+    refute user.valid?
+    assert_not_nil user.errors[:email]
+  end
 end

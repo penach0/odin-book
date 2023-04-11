79ae1fe92c7ec5540f9c0cb265254c214f24d6fc
penach0 <josemanuel.penacho@gmail.com>
2023-04-11 11:39:15 +0100

    Test Friendship model

--- test/models/friendship_test.rb
+++ test/models/friendship_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 class FriendshipTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @friendship = friendships(:one)
+  end
+
+  test "pending scope returns pending friendships" do
+    assert_includes Friendship.pending, @friendship
+
+    @friendship.accepted!
+    refute_includes Friendship.pending, @friendship
+  end
+
+  test "accepted scope returns accepted friendships" do
+    @friendship.accepted!
+    assert_includes Friendship.accepted, @friendship
+
+    @friendship.pending!
+    refute_includes Friendship.accepted, @frienship
+  end
+
+  test "finds friendship with normal order" do
+    user_id = @friendship.user_id
+    friend_id = @friendship.friend_id
+
+    result = Friendship.find_interchangeable(user_id, friend_id)
+
+    assert_equal @friendship, result
+  end
+
+  test "finds friendship with reverse order" do
+    @friendship.user = users(:two)
+    @friendship.friend = users(:one)
+
+    user_id = @friendship.user_id
+    friend_id = @friendship.friend_id
+
+    result = Friendship.find_interchangeable(user_id, friend_id)
+
+    assert_equal @friendship, result
+  end
 end

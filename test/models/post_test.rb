59dc77a4310e0f4a73792c815bfc6eaee0d1ac06
penach0 <josemanuel.penacho@gmail.com>
2023-04-11 10:54:29 +0100

    Test Post ordered scope
    - Not too happy with how it came out but I spent some time on it I'll
    improve it once I get better at testing

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class PostTest < ActiveSupport::TestCase
   def setup
-    @post = posts(:one)
+    @post = posts(:first)
   end
 
   test "valid post" do
@@ -13,5 +13,11 @@ class PostTest < ActiveSupport::TestCase
     @post.body = ""
 
     refute @post.valid?
+  end
+
+  test "posts ordered from recent to older" do
+    ordered_posts = Post.ordered
+
+    assert(ordered_posts.ordered.each_cons(2).all? { |a, b| a.created_at >= b.created_at })
   end
 end
--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -7,37 +7,38 @@ class ProfileTest < ActiveSupport::TestCase
     @incomplete_profile = profiles(:incomplete)
   end
 
-  test "valid as empty on create" do
-    empty_profile = Profile.new(user_id: users(:one).id)
+  test "should be valid when empty on create" do
+    user = users(:one)
+    empty_profile = user.build_profile
 
     assert empty_profile.valid?
   end
 
-  test "invalid on update without first name" do
+  test "should be invalid with first name empty on update" do
     @complete_profile.first_name = ""
 
     refute @complete_profile.valid?
     assert_not_nil @complete_profile.errors[:first_name]
   end
 
-  test "invalid on update without last name" do
+  test "should be invalid with last name empty on update" do
     @complete_profile.last_name = ""
 
     refute @complete_profile.valid?
     assert_not_nil @complete_profile.errors[:last_name]
   end
 
-  test "valid on update with just first and last name" do
+  test "shoud be valid on update with just first and last name given" do
     assert @incomplete_profile.valid?
   end
 
-  test "can return full name" do
+  test "#full name" do
     full_name = "#{@complete_profile.first_name} #{@complete_profile.last_name}"
 
     assert_equal @complete_profile.full_name, full_name
   end
 
-  test "can return age" do
+  test "#age" do
     age = 27
 
     assert_equal @complete_profile.age, age

ebd7e828465eac1697413332374afc91199496fa
penach0 <josemanuel.penacho@gmail.com>
2023-04-11 08:31:31 +0100

    Finish testing validations on Profile model

--- test/models/profile_test.rb
+++ test/models/profile_test.rb
@@ -1,4 +1,5 @@
 require "test_helper"
+require "pry"
 
 class ProfileTest < ActiveSupport::TestCase
   def setup
@@ -6,8 +7,7 @@ class ProfileTest < ActiveSupport::TestCase
   end
 
   test "valid as empty on create" do
-    # skip "Not sure why the profile is not valid, it works in the console"
-    empty_profile = Profile.new(user_id: 1)
+    empty_profile = Profile.new(user_id: users(:one).id)
 
     assert empty_profile.valid?
   end
@@ -17,5 +17,18 @@ class ProfileTest < ActiveSupport::TestCase
 
     refute @complete_profile.valid?
     assert_not_nil @complete_profile.errors[:first_name]
+  end
+
+  test "invalid on update without last name" do
+    @complete_profile.last_name = ""
+
+    refute @complete_profile.valid?
+    assert_not_nil @complete_profile.errors[:last_name]
+  end
+
+  test "valid on update with just first and last name" do
+    incomplete_profile = profiles(:incomplete)
+
+    assert incomplete_profile.valid?
   end
 end

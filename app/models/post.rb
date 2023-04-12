6512f7beea2e2646094440bce7038bfb83c016ae
penach0 <josemanuel.penacho@gmail.com>
2023-04-12 16:02:03 +0100

    Add active record callback to Post for broadcasts
    - I'm starting to implement real-time updates of the post index (user
    feed) through action cable

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ApplicationRecord
   validates :body, presence: true
 
   scope :ordered, -> { order(created_at: :desc) }
+
+  after_create_commit do
+    -> { broadcast_prepend_to "posts", partial: "posts/post", locals: { post: self }, target: "posts" }
+  end
 end

992c9aa2103de61bb655e7d14b8bfc1349f66096
penach0 <josemanuel.penacho@gmail.com>
2023-04-12 15:45:11 +0100

    Start testing Friendships controller
    - Ran into a problem trying to test a redirect back call that I don't
    know how to solve at the moment. I'm going to shift back to building the
    app

--- test/controllers/friendships_controller_test.rb
+++ test/controllers/friendships_controller_test.rb
@@ -1,8 +1,21 @@
 require "test_helper"
 
 class FriendshipsControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get friendships_create_url
-    assert_response :success
+  include Devise::Test::IntegrationHelpers
+
+  def setup
+    @user = users(:one)
+    @not_friend = users(:three)
+    sign_in @user
+  end
+
+  test "should create friendship between non_friends" do
+    assert_difference('Friendship.count') do
+      post friendships_url, params: { friend_id: @not_friend.id }
+    end
+
+    @request.headers["Referer"] = users_url
+
+    assert_redirected_to "www.whereicamefrom.com"
   end
 end

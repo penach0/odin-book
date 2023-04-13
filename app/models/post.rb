d4a038d1be412ae2d9728ab06c5792dbaf20d271
penach0 <josemanuel.penacho@gmail.com>
2023-04-13 16:40:13 +0100

    Broadcast Post deletions through turbo streams

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,5 @@ class Post < ApplicationRecord
 
   after_create_commit -> { broadcast_prepend_to "posts", locals: { user: Current.user } }
   after_update_commit -> { broadcast_replace_to "posts" }
+  after_destroy_commit -> { broadcast_remove_to "posts" }
 end

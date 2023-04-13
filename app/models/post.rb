ec2d60c03acf058d8ceba34a23d70c3dbca73be1
penach0 <josemanuel.penacho@gmail.com>
2023-04-13 16:03:49 +0100

    Set up passing of current_user to post partial
    - Set up everything to pass the current_user to the post variable
    so I can conditionally render the action links edit/delete, but the
    variable is not reaching the partial and I don't know why
    - I will leave the buttons being rendered for all users for now and
    focus on implementing other functionalities

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,5 @@ class Post < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
 
-  after_create_commit do
-    -> { broadcast_prepend_to "posts", partial: "posts/post", locals: { post: self }, target: "posts" }
-  end
+  after_create_commit -> { broadcast_prepend_to "posts", partial: "posts/post", locals: { post: self, user: Current.user }, target: "posts" }
 end

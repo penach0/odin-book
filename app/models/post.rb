6c029b9ebce0853464267be9a4b06a8ea502a3d6
penach0 <josemanuel.penacho@gmail.com>
2023-04-13 16:42:36 +0100

    Make Post broadcasts asynchronous

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,7 +5,7 @@ class Post < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
 
-  after_create_commit -> { broadcast_prepend_to "posts", locals: { user: Current.user } }
-  after_update_commit -> { broadcast_replace_to "posts" }
+  after_create_commit -> { broadcast_prepend_later_to "posts", locals: { user: Current.user } }
+  after_update_commit -> { broadcast_replace_later_to "posts" }
   after_destroy_commit -> { broadcast_remove_to "posts" }
 end

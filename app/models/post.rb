4b41be0c373e9fb70eda7a7c61d4c044dda83713
penach0 <josemanuel.penacho@gmail.com>
2023-04-17 11:50:22 +0100

    Add post controls for creator
    - Finally got the behaviour I wanted. I found out that the problem was
    related to conflicts with the active record `create` and `update`
    callbacks so I tried my best to remove all conflicts
    - The HTML element were the buttons are to be appended was changed
    from a turbo-frame to a normal div, this solves conflicts during editing
    the post on the spot

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,32 @@ class Post < ApplicationRecord
 
   scope :ordered, -> { order(created_at: :desc) }
 
-  broadcasts_to ->(post) { "posts" }, inserts_by: :prepend # , locals: { user: Current.user }
+  after_commit :broadcast_new_message, on: :create
+  after_commit :broadcast_edited_message, on: :update
+  after_destroy_commit -> { broadcast_remove_to "posts" }
+
+  private
+
+  def broadcast_new_message
+    broadcast_prepend_later_to "posts"
+    broadcast_controls
+  end
+
+  def broadcast_edited_message
+    return unless updated?
+
+    broadcast_replace_to "posts"
+    broadcast_controls
+  end
+
+  def broadcast_controls
+    broadcast_append_later_to [creator, "posts"],
+                              partial: "posts/creator_controls",
+                              locals: { post: self, user: creator, show_page: false },
+                              target: "creator_controls_#{id}"
+  end
+
+  def updated?
+    created_at != updated_at
+  end
 end

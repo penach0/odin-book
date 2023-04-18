3332be77dd92f15f5a42c8876a43bce76517effe
penach0 <josemanuel.penacho@gmail.com>
2023-04-18 16:31:56 +0100

    Revert Puma config changes
    - I'm finding better luck working with facebook locally over normal
    http instead of https. The config for SSL is still there just commented
    out

--- config/puma.rb
+++ config/puma.rb
@@ -14,15 +14,15 @@ threads min_threads_count, max_threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-#
-#port ENV.fetch("PORT") { 3000 }
+
+port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-#environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-#pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -41,21 +41,21 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # SSL config
 
-rackup      DefaultRackup
-port        3001
-environment ENV['RAILS_ENV'] || 'production'
+# rackup      DefaultRackup
+# port        3001
+# environment ENV['RAILS_ENV'] || 'production'
 
-if ENV['RAILS_ENV'] == 'development'
+# if ENV['RAILS_ENV'] == 'development'
 
-  localhost_key = "#{File.join('config', 'certs', 'localhost-key.pem')}"
-  localhost_crt = "#{File.join('config', 'certs', 'localhost.pem')}"
+#   localhost_key = "#{File.join('config', 'certs', 'localhost-key.pem')}"
+#   localhost_crt = "#{File.join('config', 'certs', 'localhost.pem')}"
 
-  ssl_bind '0.0.0.0', 3000, {
-    key: localhost_key,
-    cert: localhost_crt,
-    verify_mode: 'none'
-  }
-end
+#   ssl_bind '0.0.0.0', 3000, {
+#     key: localhost_key,
+#     cert: localhost_crt,
+#     verify_mode: 'none'
+#   }
+# end
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart

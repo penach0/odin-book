2f47e28c3114db88d49d4044a269d6fe690d8fd8
penach0 <josemanuel.penacho@gmail.com>
2023-04-19 08:27:01 +0100

    Fix bug with fill_required_info
    - The user couldn't logout unless it fill out first and last name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'friendships/create'
   devise_for :users, controllers: {
+    sessions: "users/sessions",
     registrations: "users/registrations",
     omniauth_callbacks: 'users/omniauth_callbacks'
   }

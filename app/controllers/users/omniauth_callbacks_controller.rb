55a5ba3ae9970f970fd67716505537e16a11385f
penach0 <josemanuel.penacho@gmail.com>
2023-04-19 08:48:59 +0100

    Complete login through Google
    - Add google to the User omniauth providers
    - Create an omniauth callback for google oauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,13 +2,21 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_action :verify_authenticity_token, only: :facebook
 
   def facebook
+    handle_auth("Facebook")
+  end
+
+  def google_oauth2
+    handle_auth("Google")
+  end
+
+  def handle_auth(kind)
     @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+      set_flash_message(:notice, :success, kind:) if is_navigational_format?
     else
-      session["devise.facebook_data"] = request.env["omniauth.auth"].except(:extra)
+      session["devise.auth_data"] = request.env["omniauth.auth"].except(:extra)
       redirect_to new_user_registration_url
     end
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: %i[facebook]
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
 
   after_create_commit { create_profile }
   has_many :friendships, dependent: :destroy

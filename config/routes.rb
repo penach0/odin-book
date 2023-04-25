d01b034da3e569c7fa9a086cdeaf6c8ea90669f2
penach0 <josemanuel.penacho@gmail.com>
2023-04-25 13:26:16 +0100

    Add layout related styles to comments

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resource :profile, only: [:show, :edit, :update]
     resources :posts, shallow: true do
-      resources :comments
+      resources :comments, except: [:show, :index]
     end
   end
 

680c8b1b0b80090fcf7349290e8a6688c12363d7
penach0 <josemanuel.penacho@gmail.com>
2023-04-25 10:59:38 +0100

    Create "link_to_feed" view helper
    - This helper encapsulates the logic of linking to a given user's feed.
    It seems that it is something that will be repeated very often

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def nested_dom_id(*args)
     args.map { |arg| arg.respond_to?(:to_key) ? dom_id(arg) : arg }.join('_')
   end
+
+  def link_to_feed(user)
+    link_to user.profile.full_name,
+            user_posts_path(user),
+            data: { turbo: false }
+  end
 end

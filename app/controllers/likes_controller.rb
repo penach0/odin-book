fad05e412479067e5af83e07359cee8c6f7490a0
penach0 <josemanuel.penacho@gmail.com>
2023-04-28 08:26:40 +0100

    Write #create action for Likes controller
    - I haven't set up any turbo stream response yet

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,12 @@
 class LikesController < ApplicationController
   def create
+    @likable = GlobalID::Locator.locate(params[:likable_gid])
+    @like = @likable.likes.create(liker_id: current_user.id)
+
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path) }
+      format.turbo_stream
+    end
   end
 
   def destroy

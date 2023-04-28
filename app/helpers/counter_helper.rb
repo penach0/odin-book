de4460d682998dc92f9c747678ed98bd93f16f92
penach0 <josemanuel.penacho@gmail.com>
2023-04-28 22:02:58 +0100

    Stream comments counter update
    - Had to reload the model (@post) because of the broadcasts
    - Not broadcasting the counter updates, I have to stop somewhere or I
    will end up broadcasting everything

--- app/helpers/counter_helper.rb
+++ app/helpers/counter_helper.rb
@@ -1,7 +1,8 @@
 module CounterHelper
   def stream_counter_update(model:, type:)
+    model.reload
+
     turbo_stream.update nested_dom_id(model, "#{type}_counter") do
-      p nested_dom_id(model, "#{type}_counter")
       content_tag(:span) do
         link_to "#{model.send(type).size} #{type}", "#" unless model.send(type).empty?
       end

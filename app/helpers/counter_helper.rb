43b23b912495ad83753a0f694f52ef72bd8cf722
penach0 <josemanuel.penacho@gmail.com>
2023-04-28 21:33:21 +0100

    Fix bug with broadcasts
    - Like buttons broke the broadcasting of posts and comments since the
    partial used current_user to render the initial liking status. This
    was solved by passing the current_user down form the views with care to
    not call it when it is undefined

--- app/helpers/counter_helper.rb
+++ app/helpers/counter_helper.rb
@@ -1,6 +1,7 @@
 module CounterHelper
   def stream_counter_update(model:, type:)
     turbo_stream.update nested_dom_id(model, "#{type}_counter") do
+      p nested_dom_id(model, "#{type}_counter")
       content_tag(:span) do
         link_to "#{model.send(type).size} #{type}", "#" unless model.send(type).empty?
       end

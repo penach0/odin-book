c9445093706c3c631a3bb7cc2798d115bc10870d
penach0 <josemanuel.penacho@gmail.com>
2023-04-29 10:42:40 +0100

    Create welcome_email mailer action

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,9 @@
 class UserMailer < ApplicationMailer
+  default from: "odinbook@email.com"
+
+  def welcome_email(user)
+    @user = user
+
+    mail to: @user.email, subject: "Welcome to OdinBook"
+  end
 end

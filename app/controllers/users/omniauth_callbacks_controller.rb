5ad749e16b7f58aba739f625149b528db7e80a2e
penach0 <josemanuel.penacho@gmail.com>
2023-05-01 09:15:52 +0100

    Fix GitHub OAuth problems
    - Turns out there were no problems at all, I just had my github email
    set to private so I could not initialize the user from the email

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,8 +17,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_omniauth(request.env["omniauth.auth"])
     Profile.from_omniauth(@user, request.env["omniauth.auth"])
 
-    pp request.env["omniauth.auth"]
-
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind:) if is_navigational_format?

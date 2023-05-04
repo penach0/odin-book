12cddee13d8831c43b7bd10f10b30e1415e2bce1
penach0 <josemanuel.penacho@gmail.com>
2023-05-04 16:53:34 +0100

    Create toggle function for dropdown

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -2,4 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = [ "menu" ]
+
+  toggle() {
+    if(getComputedStyle(this.menuTarget).display === "none") {
+      this.menuTarget.style.display = "block" 
+    } else {
+      this.menuTarget.style.display = "none"
+    }
+  }
 }

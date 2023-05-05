c0cabbf421c142d6497d6b30bb455de57dbdee4c
penach0 <josemanuel.penacho@gmail.com>
2023-05-05 17:11:07 +0100

    Implement autogrow functionality
    - Wrote autogrow functionality
    - Run autogrow in the connect() function - this automatically expands
    prefilled textareas (useful for edits)
    - Make all text areas autogrow

--- app/javascript/controllers/textarea_autogrow_controller.js
+++ app/javascript/controllers/textarea_autogrow_controller.js
@@ -2,5 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
+    this.autogrow()
+  }
+
+  autogrow() {
+    this.element.style.height = ""
+    this.element.style.height = `${this.element.scrollHeight}px`
   }
 }
